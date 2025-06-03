3d6660fb994d7e5e9e9e5391f8b5e2724fc5fcbf
Lee Mangold <lmangold@outlook.com>
2025-06-03 13:00:08 -0400

    Route Cleanup.

--- routes/web.php
+++ routes/web.php
@@ -23,37 +23,5 @@ Route::middleware(['auth'])->group(function () {
 });
 
 // Add Socialite routes
-// Route::get('/auth/{provider}/redirect', function (string $provider) {
-//     return Socialite::driver($provider)->redirect();
-// })->name('socialite.redirect');
-
 Route::get('auth/{provider}/redirect', '\App\Http\Controllers\Auth\AuthController@redirectToProvider')->name('socialite.redirect');
 Route::get('auth/{provider}/callback', '\App\Http\Controllers\Auth\AuthController@handleProviderCallback')->name('socialite.callback');
-
-// Route::get('/auth/{provider}/callback', function (string $provider) {
-//     try {
-//         $socialiteUser = Socialite::driver($provider)->user();
-
-//         dd($socialiteUser);
-
-//         // Find or create user
-//         $user = User::firstOrCreate(
-//             ['email' => $socialiteUser->getEmail()],
-//             [
-//                 'name' => $socialiteUser->getName(),
-//                 'password' => bcrypt(Str::random(16)),
-//                 'email_verified_at' => now(),
-//             ]
-//         );
-
-//         // Log the user in
-//         Auth::login($user);
-
-//         // Redirect to the dashboard
-//         return redirect()->to('/app');
-
-//     } catch (\Exception $e) {
-//         return redirect()->to('/app/login')
-//             ->with('error', 'Authentication failed. Please try again.');
-//     }
-// })->name('socialite.callback');
